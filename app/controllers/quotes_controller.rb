3aec8b0a2e96a19b53043b89bd2dc13943baf6bc
stephanielw <stephanielw88@gmail.com>
2014-09-17 16:07:48 +0000

    Moving the quotes form to a bootstrap modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,9 +2,10 @@ class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
 	end
-	def new
-		@quote=Quote.new
-	end
+
+
+	
+	
 	def create
 		@quote = Quote.create(quote_params)
 		if @quote.invalid?
